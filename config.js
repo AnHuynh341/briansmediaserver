6831e1d18a03fe1415b3a398cfd811192896db5a
AnHuynh <86704376+AnHuynh341@users.noreply.github.com>
2026-09-23 20:47:42 +0700

    Bust TablesDB adapter cache after update fix

--- config.js
+++ config.js
@@ -43,7 +43,7 @@ async function loadVideoTablesSdk() {
         });
     } else {
         const script = document.createElement('script');
-        script.src = 'video-tables-adapter.js?v=r2-delete-20260923-1';
+        script.src = 'video-tables-adapter.js?v=catalog-put-20260923-1';
         script.dataset.w41itVideoTablesAdapter = 'true';
         script.async = false;
 
